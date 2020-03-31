2e1d3c7c5f3bc275c3327c3c4349ceb0d0054c3f
MDAddario <michael.lindner.daddario@gmail.com>
2020-03-31 00:57:55 -0400

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,14 @@
+public class Board {
+
+    public static void main(String[] args) {
+        System.out.println("Work in progress");
+    }
+
+    private class Tile {
+
+        // Fields
+        private boolean isBomb;
+        private int numNeighbors;
+        private boolean isRevealed;
+    }
+}
